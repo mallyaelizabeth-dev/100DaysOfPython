0ff7c29e1c5b50673f5c65b170cee0e8beeb4be8
mallya <mallyaelizabeth007@gmail.com>
2024-11-08 01:41:57 +0300

    nothing to comment..

--- day002/tipCalc.py
+++ day002/tipCalc.py
@@ -18,15 +18,15 @@ eachPay = input("Each person should pay: $" + str(totalBill/noOfPeople))
 print("*********************************************************************")
 
 # And this is how my instructor went through about it.
-# print("Welcome to the tip calculator!")
-# bill = float(input("What was the total bill? $"))
-# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-# people = int(input("How many people to split the bill?"))
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
 
-# tip_as_percent = tip / 100
-# total_tip_amount = bill * tip_as_percent
-# total_bill = bill + total_tip_amount
-# bill_per_person = total_bill / people
-# final_amount = round(bill_per_person, 2)
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
 
-# print(f"Each person should pay: ${final_amount}")
+print(f"Each person should pay: ${final_amount}")
