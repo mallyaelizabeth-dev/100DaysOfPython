08eaa2678bddb9ebec4123195370cb7c6409da39
mallya <mallyaelizabeth007@gmail.com>
2024-11-08 13:10:01 +0300

    A functionality of testing whether the correct letter was choosen or not was made with the use of while loop.

--- day003/pizzaOrder.py
+++ day003/pizzaOrder.py
@@ -1,8 +1,25 @@
 print("Thank you for choosing Python Pizza Deliveries!")
 
-size = input("What pizza do you want? S, M, or L: ").upper()
-add_pepperoni = input("Do you want pepperoni? Y or N: ").upper()
-extra_cheese = input("Do you want extra cheese? Y or N: ").upper()
+# get the valid size
+def get_valid_size():
+    size = input("What pizza do you want? S, M, or L: ").upper()
+    while size not in ['S', 'M', 'L']: # Force the user to choose from the given letters.
+        print("Invalid choice! Please choose between S, M or L!")
+        size = input("What pizza do you want? S, M, or L: ").upper()
+    return size
+
+# function to get a valid Yes or No response!
+def get_valid_yes_no(question):
+    choice = input(question).upper()
+    while choice not in ['Y', 'N']: # Force the use to choose the correct response!
+        print("Invalid input! Please choose either Y or N.")
+        choice = input(question).upper()
+    return choice
+    
+size = get_valid_size()
+
+add_pepperoni = get_valid_yes_no("Do you want pepperoni? Y or N: ").upper()
+extra_cheese = get_valid_yes_no("Do you want extra cheese? Y or N: ").upper()
 
 # initilizae the bill
 bill = 0
@@ -11,9 +28,9 @@ bill = 0
 if size == "S":
     bill = 15
 elif size == "M":
-    bill == 20
+    bill = 20
 elif size == "L":
-    bill == 25 
+    bill = 25 
 
 # Add pepperoni
 
@@ -22,12 +39,13 @@ if add_pepperoni == "Y":
         bill+=2 # for small pizza
     else:
         bill += 3 # for medium and large pizza
-else:
-    print("Unkown selection")    
+
 # Add extra cheese 
 if extra_cheese == "Y":
     bill += 1
-
+    
+    
+    
 # Final bill
 print(f"Your final pizza bill is {bill}")
 
